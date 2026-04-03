570fe0cd575c3e403084486761c71b232b83d11c
Louis Atkinson <louisatkinson.la@gmail.com>
2026-04-03 07:36:06 +0100

    auth request dtos

---
+++ src/main/java/com/chess/dto/request/AuthRequests.java
@@ -0,0 +1,39 @@
+package com.chess.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+public class AuthRequests {
+
+    @Data
+    public static class RegisterRequest {
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, max = 50, message = "Username must be 3-50 characters")
+        private String username;
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        private String email;
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, max = 100, message = "Password must be 8-100 characters")
+        private String password;
+    }
+
+    @Data
+    public static class LoginRequest {
+        @NotBlank(message = "Username is required")
+        private String username;
+
+        @NotBlank(message = "Password is required")
+        private String password;
+    }
+
+    @Data
+    public static class RefreshRequest {
+        @NotBlank
+        private String refreshToken;
+    }
+}
